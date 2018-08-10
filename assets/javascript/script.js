5337aafe1f9c216717db39180cb508e8a12c0350
Mark Fisher <mfisher36@gmail.com>
2018-08-10 15:51:17 -0400

    working on keypress

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,7 +2,7 @@ var gameVars = {
   gameStart: false,
   gameOver: false,
   wins: 0,
-  wordPool: ["JAVASCRIPT", "HTML", "CSS", "CODE", "JQUERY"], 
+  wordPool: ["JAVASCRIPT", "HTML", "CSS", "PSEUDOCODE", "JQUERY", "ARRAY", "BOOTSTRAP", "OBJECT", "FUNCTION"], 
   currentWord: "",
   guessesRemaining: 10,
   guessedChars: [],
@@ -11,14 +11,19 @@ var gameVars = {
 
 var gameFuncs = {
 
-  setWordAndBlanks: function() {
+  resetGame: function() {
+    gameVars.blanks = [];
     gameVars.currentWord = gameVars.wordPool[Math.floor((Math.random() * gameVars.wordPool.length) + 0)];
+    
+    //TEST
+    document.querySelector("#word").innerHTML = gameVars.currentWord;
+    
     for (var i = 0; i < gameVars.currentWord.length; i++) {
       gameVars.blanks.push("_ ");
     }
     this.maintainBlanks();
   },
-
+  
   maintainBlanks: function() {
     document.querySelector("#blanks").innerHTML = "<h1>" + gameVars.blanks.join("") + "<h1>";
   },
@@ -32,16 +37,32 @@ var gameFuncs = {
         }
       }
     } else {
-      gameVars.guessesRemaining--;
-      gameVars.guessedChars.push(guess);
-      // Feature: Picture changes
+
+
+      if (gameVars.guessesRemaining === 0) {
+        setTimeout(function() {
+          if (!gameVars.blanks.join("").includes("_")) {
+            alert("YOU LOSE AND ARE BANISHED TO THE UNDERWORLD FOREVER");
+            gameFuncs.resetGame();}
+          }, 0);
+      } else {
+        // Feature: Picture changes
+        if (!gameVars.guessedChars.includes(guess)) {
+          gameVars.guessedChars.push(guess);
+          gameVars.guessesRemaining--;
+          document.querySelector("#guessesRemaining").innerHTML = gameVars.guessesRemaining;
+          console.log(gameVars.guessedChars.join());
+          document.querySelector("#guessed").innerHTML = gameVars.guessedChars;
+        }
+
+      }
     }
   }
 
 }
 
 //Creates word and sets 'blanks' variable
-gameFuncs.setWordAndBlanks();
+gameFuncs.resetGame();
 
 
 //TEST
@@ -51,14 +72,18 @@ document.querySelector("#word").innerHTML = word;
 // Event Listeners:
 //When a key is pressed, start 'gamePlay' function
 document.onkeyup = function(event) {
-  gameVars.gameStart = true;
-  var guessedLetter = event.key.toUpperCase();
-  gameFuncs.gamePlay(guessedLetter);
-
+  if (/[a-zA-Z]/.test(event.key)) {
+    gameVars.gameStart = true;
+    var guessedLetter = event.key.toUpperCase();
+    gameFuncs.gamePlay(guessedLetter);
+  }
   setTimeout(function() {
     if (!gameVars.blanks.join("").includes("_")) {
-      alert("YOU WIN!");
+      alert("YOU WIN! PRESS SPACEBAR TO PLAY AGAIN");
+      gameFuncs.resetGame();
     }
   }, 0);
   
+
+
 }
